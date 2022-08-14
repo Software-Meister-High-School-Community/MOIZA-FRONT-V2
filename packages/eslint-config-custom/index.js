0f23b3bdc184c8233c9084b3208dc45db3675d14
정우재 <80890472+woojae05@users.noreply.github.com>
2022-08-14 23:04:04 +0900

    setup: eslint

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,56 @@
 module.exports = {
+  env: {
+    browser: true,
+    node: true,
+  },
   extends: ["next", "turbo", "prettier"],
+
+  extends: [
+    "next",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "prettier",
+  ],
+  plugins: ["@typescript-eslint", "import"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
   },
+  settings: {
+    next: {
+      rootDir: ["apps/*/", "packages/*/"],
+    },
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true,
+        project: ["apps/*/tsconfig.json"],
+      },
+    },
+  },
+  rules: {
+    // react
+    "react/function-component-definition": [
+      2,
+      {
+        namedComponents: "arrow-function",
+      },
+    ],
+
+    // next
+    "@next/next/no-html-link-for-pages": "off",
+  },
+  ignorePatterns: [
+    "**/*.js",
+    "**/*.json",
+    "node_modules",
+    "public",
+    "styles",
+    ".next",
+    "coverage",
+    "dist",
+    ".turbo",
+  ],
 };
